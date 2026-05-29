445939324e2e0fb065d66403280bfe7af3b8835d
0926695444 <yo.mohammadpanah@gmail.com>
2026-05-29 19:53:02 +0330

    developing register api; creating user and hashing password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const userModel = require("./../models/user");
+const bcrypt = require("bcrypt");
 const registerValidator = require("./../validators/register");
 
 exports.register = async (req, res) => {
@@ -12,6 +13,17 @@ exports.register = async (req, res) => {
   if (isUserExists) {
     res.status(409).json({ message: "This username or email already existed" });
   }
+
+  const countOfUser = await userModel.count();
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = userModel.create({
+    email,
+    username,
+    name,
+    phone,
+    password: hashedPassword,
+    role: countOfUser > 0 ? "USER" : "ADMIN",
+  });
 };
 
 exports.login = async (req, res) => {};
