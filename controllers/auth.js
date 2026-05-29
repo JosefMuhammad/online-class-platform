ecbd04be8630a5b633d3cbea6d4099c8a23147d7
0926695444 <yo.mohammadpanah@gmail.com>
2026-05-29 19:08:40 +0330

    implementing register api

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,18 @@
 const userModel = require("./../models/user");
+const registerValidator = require("./../validators/register");
 
-exports.register = async (req, res) => {};
+exports.register = async (req, res) => {
+  const validationResult = registerValidator(req.body);
+  if (validationResult != true) {
+    return res.status(422).json(validationResult);
+  }
+
+  const { username, name, email, password, phone } = req.body;
+  const isUserExists = userModel.findOne({ $or: [{ username }, { email }] });
+  if (isUserExists) {
+    res.status(409).json({ message: "This username or email already existed" });
+  }
+};
 
 exports.login = async (req, res) => {};
 
