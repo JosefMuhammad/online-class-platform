d48ee037f8c36d9e2f9f3dedf145edf82b5ec38d
0926695444 <yo.mohammadpanah@gmail.com>
2026-05-29 11:28:01 +0330

    validating user for register

---
+++ validators/register.js
@@ -0,0 +1,15 @@
+const validator = require("fastest-validator");
+const v = new validator();
+
+const schema = {
+  username: { type: "string", min: 3, max: 150 },
+  name: { type: "string", min: 3, max: 150 },
+  email: { type: "email", min: 10, max: 150 },
+  password: { type: "string", min: 10, max: 30 },
+  confirmPassword: { type: "equal", field: "password" },
+  $$strinct: true,
+};
+
+const check = v.compile(schema);
+
+module.exports(check);
