7ab0c34b6d29395b74c34c7624ffefeffcaefb24
0926695444 <yo.mohammadpanah@gmail.com>
2026-05-29 11:53:43 +0330

    adding essential middlewares

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 //This file is for handling router and middlewares
 const express = require("express");
 const app = express();
+const authRouter = require("./routes/auth");
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  "/courses/covers",
+  express.static(path.join(__dirname, "public", "courses", "covers")),
+);
+app.use("/auth", authRouter);
+
 module.exports = app;
