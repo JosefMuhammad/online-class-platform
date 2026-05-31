405a47fe2b81273ddc5855c79ef231a03cd42484
0926695444 <yo.mohammadpanah@gmail.com>
2026-05-31 20:48:20 +0330

    remove limitatipn for phone property

--- validators/register.js
+++ validators/register.js
@@ -5,7 +5,7 @@ const schema = {
   username: { type: "string", min: 3, max: 150 },
   name: { type: "string", min: 3, max: 150 },
   email: { type: "email", min: 10, max: 150 },
-  phone: { type: "number", max: 11 },
+  phone: { type: "string" },
   password: { type: "string", min: 10, max: 30 },
   confirmPassword: { type: "equal", field: "password" },
   $$strinct: true,
